19a310e238c91e2131d73c3a3a2bb7aa083cd7a4
jane <82953024+git-jane-hub@users.noreply.github.com>
2021-08-14 23:48:12 +0900

    Review4

--- src/kr/co/abc/board/service/R06Board_delete_service.java
+++ src/kr/co/abc/board/service/R06Board_delete_service.java
@@ -3,7 +3,7 @@ package kr.co.abc.board.service;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class R05Board_update_service {
+public class R06Board_delete_service implements R01Service{
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
